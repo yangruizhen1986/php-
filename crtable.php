2fd37e0deb70b9ebeaf13a1dc901b91a7ebd1cc1
yangruizhen1986 <514971707@qq.com>
2017-07-25 11:25:18 +0800

    数据库创建mydb 表admin 管理员注册页面

---
+++ crtable.php
@@ -0,0 +1,24 @@
+﻿<?php
+$servername="localhost";
+$username="root";
+$password="root";
+$dbname="myDB";
+//创建连接
+$conn=new mysqli($servername,$username,$password,$dbname);
+//测试连接
+if($conn->connect_error){
+	die("连接失败：".$conn->connect_error);
+}
+$sql="CREATE TABLE admin(
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+user VARCHAR(30) NOT NULL,
+pass VARCHAR(30) NOT NULL,
+reg_date TIMESTAMP
+)";
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "创建数据表错误: " . $conn->error;
+}
+$conn->close();
+?>
---
+++ reg.php
@@ -0,0 +1,38 @@
+﻿<?php
+/*
+$user = $_POST["username"];
+$pass = $_POST["password"];
+$conn= new mysqli("localhost","root","root","mydb");
+if($conn->connect_error){
+	die("连接错误：".$conn->connect_error);
+}
+
+$stmt= $conn->prepare("INSERT INTO admin(user,pass)VALUES(?,?)");
+$stmt->bind_param("ss",$user,$pass);
+$stmt->execute();
+echo "注册成功！";
+
+$stmt->close();
+$conn->close();
+上面这种注册只是注册！*/
+$user = $_POST["username"];
+$pass = $_POST["password"];
+$conn = new mysqli("localhost","root","root","mydb");
+if($conn->connect_error){
+	die("连接错误：".$conn->connect_error);
+}
+$sql = "SELECT * FROM admin where user=".$_POST["username"];
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+	echo("用户名存在");
+}
+else{
+$stmt= $conn->prepare("INSERT INTO admin(user,pass)VALUES(?,?)");
+$stmt->bind_param("ss",$user,$pass);
+$stmt->execute();
+echo "注册成功！";
+$stmt->close();
+}
+
+$conn->close();
+?>
